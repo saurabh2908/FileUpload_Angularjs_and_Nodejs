5bba431c520de665fc84dfcaf465fcc8e349560b
Saurabh singh <2908aman@gmail.com>
2019-06-15 00:37:04 +0530

    Multer File Upload

---
+++ uploadDemo/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      console.log('Going to Store the Data in Disk');
+      cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+      console.log('File name is ',file.fieldname);
+      cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+      console.log(path.extname(file.originalname));
+    }
+  })
+  
+  
+
+app.use( multer({ storage: storage }).single('file'));
+app.post('/upload',(request, response)=>{
+  console.log('Server Upload ......');
+        response.json({'msg':'File Uploaded ....'});
+})
+app.listen(1234,()=>{
+  console.log('Server Started...');
+})
